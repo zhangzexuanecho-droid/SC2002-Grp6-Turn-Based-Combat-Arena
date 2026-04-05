b590e364970e71ea9b2fe56a188773f5b86e04d3
anzhizhao6-design <anzhizhao6@gmail.com>
2026-04-05 16:09:04 +0800

    Update Warrior.java

--- src/combat/Warrior.java
+++ src/combat/Warrior.java
@@ -8,9 +8,34 @@ public class Warrior extends Player {
         super("Warrior", 260, 40, 20, 30);
     }
 
+    @Override
     public Action chooseAction() {
-        // to be inplemented later
-        return null
+        System.out.println("\n--- Choose your action ---");
+        System.out.println("1. Basic Attack");
+        System.out.println("2. Defend");
+        System.out.println("3. Use Item");
+        
+   
+        if (this.skillCooldown == 0) {
+            System.out.println("4. Special Skill (Shield Bash)");
+        } 
+        else {
+            System.out.println("4. [Skill Cooldown: " + skillCooldown + " turns]");
+        }
+
+        int choice = scanner.nextInt();
+    
+        switch (choice) {
+            case 1: return new BasicAttack();
+            case 2: return new Defend(); 
+            case 3: return UseItem(); 
+            case 4: 
+                if (this.skillCooldown == 0) return new ShieldBash();
+                else System.out.println("Skill not ready!");
+            default: 
+                System.out.println("Invalid choice, defaulting to Attack.");
+                return new BasicAttack();
+        }
     }
 
     public void useSpecialSkill(Combatant target) {
