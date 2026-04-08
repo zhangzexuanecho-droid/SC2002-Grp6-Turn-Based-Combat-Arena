d80c057522e927cf3c97d2176de35cb1eab690e8
shihuangg <shihuangh@gmail.com>
2026-04-08 15:04:39 +0800

    Update Potion.java

--- src/item/Potion.java
+++ src/item/Potion.java
@@ -1,1 +1,29 @@
+package item;
 
+import combat.Combatant;
+
+public class Potion implements Item{
+  private int healAmount;
+
+  public Potion(){
+    this.healAmount = 100;
+  }
+  @Override
+  public void use(Combatant user){
+    int beforeHp = user.getHp();
+    int afterHp = beforeHp + healAmount;
+
+    if (afterHp > user.getMaxHp()){
+      afterHo = user.getMaxHp();
+    }
+    user.setHp(afterHp);
+
+    System.out.println(user.getName() + " -> Item -> Potion used: HP: " + beforeHp + " -> " + afterHp + " (+" + (afterHp - beforeHp) + ")");
+                      
+  
+  @Override
+  public String getName(){
+    return "Potion";
+
+  }
+}
