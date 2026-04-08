1252251a18c7c05ba72222bba8061e941672806e
shihuangg <shihuangh@gmail.com>
2026-04-08 23:29:16 +0800

    Create StunEffect.java

---
+++ src/combat/StunEffect.java
@@ -0,0 +1,27 @@
+package combat;
+
+public class StunEffect implements StatusEffect {
+
+    private int remainingTurns;
+
+    public StunEffect(int turns) {
+        this.remainingTurns = turns;
+    }
+
+    @Override
+    public void apply(Combatant target) {
+        if (remainingTurns > 0) {
+            System.out.println(target.getName() + " → STUNNED: Turn skipped");
+            remainingTurns--;
+        }
+    }
+
+    @Override
+    public boolean isExpired() {
+        return remainingTurns <= 0;
+    }
+
+    public boolean isActive() {
+        return remainingTurns > 0;
+    }
+}
