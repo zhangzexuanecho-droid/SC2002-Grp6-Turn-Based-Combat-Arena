87d1f51d6ae820af2ac4b6294efe4c749a5c5152
vkaadhira-gif <vkaadhira@gmail.com>
2026-04-10 00:10:53 +0800

    Create UseItem.java

---
+++ src/UseItem.java
@@ -0,0 +1,23 @@
+package action;
+
+import combat.Combatant;
+import item.Item;
+
+public class UseItem implements Action 
+{
+
+    private Item item;
+
+    public UseItem(Item item) 
+    {
+        this.item = item;
+    }
+
+    @Override
+    public void execute(Combatant user, Combatant target) 
+    {
+        item.use(user);
+
+        //System.out.println(user.getName() + " uses an item.");
+    }
+}
