654e60b7e369d43947f146d61f4aa11861233449
vkaadhira-gif <vkaadhira@gmail.com>
2026-04-10 00:08:07 +0800

    Create Defend.java

---
+++ src/Defend.java
@@ -0,0 +1,34 @@
+package action;
+
+import combat.Combatant;
+import combat.StatusEffect;
+
+public class Defend implements Action 
+{
+
+    @Override
+    public void execute(Combatant user, Combatant target) 
+    {
+
+        user.addStatusEffect(new StatusEffect() 
+        {
+
+            private int duration = 2;
+
+            @Override
+            public void apply(Combatant target) 
+            {
+                target.defense += 10; // increase defense
+                duration--;
+            }
+
+            @Override
+            public boolean isExpired() 
+            {
+                return duration <= 0;
+            }
+        });
+
+        //System.out.println(user.getName() + " is defending!");
+    }
+}
