043754fbe6fab1eedefc29fd9ee526972d02fbcc
shihuangg <shihuangh@gmail.com>
2026-04-10 12:14:06 +0800

    Update PowerStone.java

--- src/item/PowerStone.java
+++ src/item/PowerStone.java
@@ -4,36 +4,17 @@ import combat.Combatant;
 
 public class PowerStone implements Item {
 
-    private int atkBoost;
-    private int duration;
-
     public PowerStone(String difficulty) {
-        switch (difficulty.toLowerCase()) {
-            case "easy": atkBoost = 20; duration = 1; break;
-            case "medium": atkBoost = 20; duration = 1; break;
-            case "hard": atkBoost = 30; duration = 1; break;
-            default: atkBoost = 20; duration = 1;
-        }
+        
     }
 
     @Override
     public void use(Combatant user) {
-        
-        System.out.println(user.getName() + " → Item → Power Stone used: "
-                + "ATK increased by " + atkBoost + " for " + duration + " turn"
-                + (duration > 1 ? "s" : ""));
+        System.out.println(user.getName() + " → Item → Power Stone used");
     }
 
     @Override
     public String getName() {
         return "Power Stone";
-    }
-
-    public int getAtkBoost() {
-        return atkBoost;
-    }
-
-    public int getDuration() {
-        return duration;
     }
 }
