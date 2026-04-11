5f7889e490ec74af71d8066c2b4f69e061cc0d8e
anzhizhao6-design <anzhizhao6@gmail.com>
2026-04-11 17:33:50 +0800

    Update Enemy.java

--- src/combat/Enemy.java
+++ src/combat/Enemy.java
@@ -15,16 +15,25 @@ public abstract class Enemy extends Combatant {
 
     public Action decideAction() {
         return new BasicAttack();
+    }
     
    @Override
     public void takeTurn(BattleEngine engine, GameUI ui) {
 
         List<Combatant> targets = engine.getAliveEnemiesOf(this);
         if (targets.isEmpty()) return;
+       
+       /*
+        boolean canAct = processTurnStartEffects();
+        if (!canAct) {
+            System.out.println(getName() + " cannot act this turn!");
+            return;
+        }
+       */
 
         Combatant target = targets.get(0); // simplest
 
-        Action action = decideAction(); // ✅ 用这个
+        Action action = decideAction(); 
 
         if (action.requiresTarget()) {
             action.execute(this, target);
