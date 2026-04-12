ae1be17c8dbffe50d2985e571116718096ad5503
shihuangg <shihuangh@gmail.com>
2026-04-12 23:34:23 +0800

    Update StatusEffect.java

--- src/combat/statusEffect/StatusEffect.java
+++ src/combat/statusEffect/StatusEffect.java
@@ -1,5 +1,7 @@
 package combat.statusEffect;
 public interface StatusEffect{
   void apply(Combatant target);
+  boolean preventsAction();
+  int modifyIncomingDamage(int damage);
   boolean isExpired();
 }
