782c385bf7d4e1117e1eb7ac6476f15deeaab2ad
anzhizhao6-design <anzhizhao6@gmail.com>
2026-04-18 18:15:22 +0800

    Update BattleEngine.java

--- src/BattleEngine/BattleEngine.java
+++ src/BattleEngine/BattleEngine.java
@@ -1,10 +1,10 @@
 package BattleEngine;
 
-import java.util.List;
-
 import combat.Combatant;
 import combat.Enemy;
 import combat.Player;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BattleEngine {
 
@@ -23,28 +23,11 @@ public class BattleEngine {
         while (!checkGameEnd()) {
             processRound();
         }
-
-        int remainingHP = 0;
-        int enemiesRemaining = 0;
-
-        for (Combatant c : combatants) {
-            if (c.isAlive()) {
-                if (c instanceof Player) {
-                    remainingHP = c.getHp();
-                } else if (c instanceof Enemy) {
-                    enemiesRemaining++;
-                }
-            }
-        }
-
-        if (hasPlayerAlive()) {
-            this.ui.displayPlayerVictory(remainingHP, currentRound) ;
-        } else {
-            this.ui.displayPlayerDefeat(enemiesRemaining, currentRound);
-        }
     }
 
     public void processRound() {
+        ui.displayRoundStart(currentRound);
+
         List<Combatant> turnOrder = turnStrategy.determineOrder(combatants);
 
         for (Combatant currentCombatant : turnOrder) {
@@ -52,16 +35,27 @@ public class BattleEngine {
                 break;
             }
 
-            if (currentCombatant.isAlive()) {
-                currentCombatant.applyStatusEffects();
+            if (!currentCombatant.isAlive()) {
+                continue;
+            }
 
-                if (currentCombatant.isAlive()) {
-                    currentCombatant.takeTurn(this, ui);
-                    processTurn();
-                }
+            boolean canAct = currentCombatant.updateStatusEffects();
+
+            if (!currentCombatant.isAlive()) {
+                processTurn();
+                continue;
+            }
+
+            if (canAct) {
+                currentCombatant.takeTurn(this, ui);
+                processTurn();
+            } else {
+                ui.showMessage(currentCombatant.getName() + " cannot act this turn!");
+                processTurn();
             }
         }
-        this.currentRound++;
+
+        currentRound++;
     }
 
     public boolean checkGameEnd() {
@@ -87,11 +81,30 @@ public class BattleEngine {
     }
 
     public void processTurn() {
+        List<Combatant> defeated = new ArrayList<>();
+
+        for (Combatant c : combatants) {
+            if (!c.isAlive()) {
+                defeated.add(c);
+            }
+        }
+
+        Player player = getPlayer();
+
+        for (Combatant defeatedCombatant : defeated) {
+            if (defeatedCombatant instanceof Enemy) {
+                int remainingHP = (player != null && player.isAlive()) ? player.getHp() : 0;
+                ui.displayEnemyDefeated(defeatedCombatant.getName(), remainingHP);
+            } else {
+                ui.displayCombatantEliminated(defeatedCombatant.getName());
+            }
+        }
+
         combatants.removeIf(c -> !c.isAlive());
     }
 
     public List<Combatant> getAliveEnemiesOf(Combatant self) {
-        List<Combatant> result = new java.util.ArrayList<>();
+        List<Combatant> result = new ArrayList<>();
 
         if (self instanceof Player) {
             for (Combatant c : combatants) {
@@ -112,5 +125,28 @@ public class BattleEngine {
 
     public List<Combatant> getCombatants() {
         return combatants;
+    }
+
+    public int getCurrentRound() {
+        return currentRound - 1;
+    }
+
+    public int getEnemiesRemaining() {
+        int count = 0;
+        for (Combatant c : combatants) {
+            if (c instanceof Enemy && c.isAlive()) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public Player getPlayer() {
+        for (Combatant c : combatants) {
+            if (c instanceof Player) {
+                return (Player) c;
+            }
+        }
+        return null;
     }
 }
